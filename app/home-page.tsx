4b6c9815757fe9531c780588c2f0e1cf8aa8756c
theredfish <did.julian@gmail.com>
2023-10-01 17:45:08 +0200

    Update Indiedevcasts topics

--- app/home-page.tsx
+++ app/home-page.tsx
@@ -37,13 +37,13 @@ function Articles({ recentPosts }) {
 function HeroBanner() {
   return (
     <div className="bg-indigo-500 w-full max-w-full text-gray-100 my-14 py-10 sm:py-20">
-      <div className="blog-container flex flex-col sm:flex-row space-y-10 sm:space-y-0 sm:space-x-10">
-        <p className="w-full sm:w-1/3 text-2xl lg:text-3xl sm:font-medium">
-          Indiedevcasts is a project where I share gamedev content with the
-          community.
+      <div className="blog-container flex items-center flex-col sm:flex-row space-y-10 sm:space-y-0 sm:space-x-10">
+        <p className="w-full md:w-1/3 text-2xl lg:text-3xl md:font-medium">
+          Indiedevcasts is a place where I broadcast content about software and
+          game development.
         </p>
-        <p className="w-full sm:w-2/3 text-4xl lg:text-6xl">
-          Devlogs, articles, videos and games.
+        <p className="w-full  md:w-2/3 text-4xl lg:text-6xl align-middle">
+          Indie hacking, SaaS, games, voxel art and even aeroponics!
         </p>
       </div>
     </div>
@@ -54,30 +54,84 @@ function AboutIndiedevcasts() {
   return (
     <section className="blog-container mt-28">
       <div className="relative bg-overlapping-circles w-full lg:w-10/12 text-white rounded-md">
-        <div className="w-full lg:w-3/5 xl:w-1/2 p-5 lg:p-10">
+        <div className="w-full lg:w-3/5 lg:p-10 xl:w-1/2 p-5">
           <h3 className="text-3xl sm:text-4xl lg:text-6xl">
             About Indiedevcasts
           </h3>
           <p className="mt-10 text-lg lg:text-xl">
-            My name is Julian and Indiedevcasts is a side-project where I share
-            gamedev material with my community. I focus my content on{" "}
-            <a href="/" target="_blank" className="text-gray-100">
-              Rust
-            </a>{" "}
-            programming language,{" "}
-            <a href="/" target="_blank" className="text-gray-100">
-              Bevy engine
-            </a>{" "}
-            , procedural generation and voxel art. I used to work with Unity and
-            C# before, and future content might target that environment as well!
+            My name is Julian and Indiedevcasts is a place where I produce
+            content about software and game development for my community. Here
+            are the technologies I use regularly:
           </p>
 
+          <ul className="mt-5 list-disc list-inside text-lg lg:text-xl">
+            <li>
+              <a
+                href="https://www.rust-lang.org"
+                target="_blank"
+                className="text-gray-100"
+              >
+                Rust
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.postgresql.org"
+                target="_blank"
+                className="text-gray-100"
+              >
+                PostgreSQL
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://nextjs.org"
+                target="_blank"
+                className="text-gray-100"
+              >
+                Next.js
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://tailwindcss.com"
+                target="_blank"
+                className="text-gray-100"
+              >
+                Tailwind CSS
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://bevyengine.org"
+                target="_blank"
+                className="text-gray-100"
+              >
+                Bevy Engine
+              </a>
+              ,{" "}
+              <a
+                href="https://ephtracy.github.io/index.html?page=mv_main"
+                target="_blank"
+                className="text-gray-100"
+              >
+                MagicaVoxel
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://www.blender.org"
+                target="_blank"
+                className="text-gray-100"
+              >
+                Blender
+              </a>
+            </li>
+          </ul>
+
           <p className="mt-10 text-lg lg:text-xl">
-            Indiedevcasts is a community-centric project. That's why this
-            website will evolve over time to provide members with different
-            features and content revolving around gamedev. I dedicate a few
-            hours a week plus my weekends to developing games, editing videos
-            and writing blog posts.
+            What's the meaning of Indiedevcasts? Thanks for asking! I'm an
+            indiependent developer who broadcasts content to an audience in
+            different formats such as screencasts, podcasts or written material.
+            Check my different communication channels below!
           </p>
 
           <p className="mt-10 text-xl"></p>
@@ -93,20 +147,19 @@ function AboutIndiedevcasts() {
               Stay up to date
             </h3>
             <p className="mt-3 text-lg lg:text-xl">
-              The newsletter is the best way for you to stay up to date with the
-              latest content published here or on my social network, and for me
-              to build my community!
+              The newsletter is the best way for you to keep up with the latest
+              content published here or on my social networks.
             </p>
           </div>
 
-          <div className="mt-6">
+          <div className="mt-10">
             <h3 className="inline-flex items-center text-xl sm:text-2xl font-semibold">
               <ChatBubbleLeftRightIcon className="h-5 sm:h-6 mr-1" />
               Community content
             </h3>
             <p className="mt-3 text-lg lg:text-xl">
               I highlight games, tools, projects and videos coming from the
-              community. It's a great way to discover new gamedev content or
+              community. It's a great way to discover new indiedev content or
               showcase your own (
               <a
                 href="mailto:hello@indiedevcasts.com"
@@ -117,16 +170,17 @@ function AboutIndiedevcasts() {
               ).
             </p>
           </div>
-          <div className="mt-6">
+          <div className="mt-10">
             <h3 className="inline-flex items-center text-xl sm:text-2xl font-semibold">
-              <BellAlertIcon className="h-5 sm:h-6 mr-1" /> Don't miss my games
+              <BellAlertIcon className="h-5 sm:h-6 mr-1" /> Support my work
             </h3>
             <p className="mt-3 text-lg lg:text-xl">
               You can also subscribe to promotional content. Don't miss my
-              games, special offers, or gamedev tools by ticking the checkbox !
+              games, services, special offers, or gamedev tools by ticking the
+              checkbox.
             </p>
           </div>
-          <div className="flex items-center space-x-5 mt-10">
+          <div className="flex items-center space-x-5 mt-5 md:mt-10 lg:mt-16">
             <a
               href="https://mailchi.mp/e1b370d548a1/indiedevcasts-newsletter"
               target="_blank"
@@ -134,13 +188,13 @@ function AboutIndiedevcasts() {
             >
               Subscribe
             </a>
-            {/* <a
-              href="https://mailchi.mp/e1b370d548a1/indiedevcasts-newsletter"
+            <a
+              href="https://us4.campaign-archive.com/home/?u=d1ce365b7fa38a948561b8785&id=f06c9cb23b"
               target="_blank"
-              className="font-semibold  text-white hover:text-white hover:border-b hover:border-white text-xl border-b-0"
+              className="font-semibold text-white hover:text-white hover:border-b hover:border-white text-xl border-b-0"
             >
-              Consult archives
-            </a> */}
+              Past issues
+            </a>
           </div>
         </div>
       </div>
